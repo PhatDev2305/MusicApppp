4087a72d4588961ec915dfbcf2d961b2160b6ff6
PhatDev2305 <leagea10@gmail.com>
2020-11-08 20:22:43 +0700

    Sua file

---
+++ music-app/build/precache-manifest.2cffdaae163da8fe70bdb1887965cccc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36f15682d77f4d8248dd36464bad94c",
+    "url": "/MusicApppp/index.html"
+  },
+  {
+    "revision": "fc55fa9ead03196da4db",
+    "url": "/MusicApppp/static/css/main.91ccef42.chunk.css"
+  },
+  {
+    "revision": "9c26b37d31a3f695fa44",
+    "url": "/MusicApppp/static/js/2.a130edc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MusicApppp/static/js/2.a130edc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc55fa9ead03196da4db",
+    "url": "/MusicApppp/static/js/main.b539a003.chunk.js"
+  },
+  {
+    "revision": "4c28754116ec29ec5d16",
+    "url": "/MusicApppp/static/js/runtime-main.771262a8.js"
+  },
+  {
+    "revision": "2700dac0ab177771676066d0ce692b57",
+    "url": "/MusicApppp/static/media/DeathBed.2700dac0.mp3"
+  },
+  {
+    "revision": "557ae9bf95ceaf70a24c2f642a8268d4",
+    "url": "/MusicApppp/static/media/HongDamDau.557ae9bf.mp3"
+  },
+  {
+    "revision": "80877fb074f0786d4fdc048580e4d312",
+    "url": "/MusicApppp/static/media/KhongDamDau.80877fb0.jpg"
+  },
+  {
+    "revision": "89b1d297ca069a040aa11b712b61ea8e",
+    "url": "/MusicApppp/static/media/SummerTime.89b1d297.mp3"
+  },
+  {
+    "revision": "b163eb6fe4bc5660b4c6a96950317d4a",
+    "url": "/MusicApppp/static/media/SummerTime.b163eb6f.jpg"
+  },
+  {
+    "revision": "8111d08655fb016af673b05e0c734c48",
+    "url": "/MusicApppp/static/media/VayHoaNhi.8111d086.jpg"
+  },
+  {
+    "revision": "a6512b9847c2a614442893f16b428ee5",
+    "url": "/MusicApppp/static/media/VayHoaNhi.a6512b98.mp3"
+  }
+]);
